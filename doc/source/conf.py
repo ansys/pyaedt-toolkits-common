c64798812d0e329a72ed93e41550a748f816a4ed
Sebastien Morais <sebastien.morais@ansys.com>
2024-03-12 10:21:35 +0100

    DOC: Add PDF download button

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -139,6 +139,11 @@ html_theme_options = {
             "url": "https://github.com/ansys/pyaedt-toolkits-common/issues",
             "icon": "fa fa-comment fa-fw",
         },
+        {
+            "name": "Download documentation in PDF",
+            "url": f"https://{cname}/version/{switcher_version}/_static/assets/download/ansys-aedt-toolkits-common.pdf",  # noqa: E501
+            "icon": "fa fa-file-pdf fa-fw",
+        },
     ],
     "navigation_with_keys": False,
 }
