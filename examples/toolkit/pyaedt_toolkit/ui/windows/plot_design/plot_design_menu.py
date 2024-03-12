78607a3f98a9f0fabbefda3bd3ae84300943fba3
Giulia Malinverno <giulia.malinverno@ansys.com>
2024-03-12 15:02:25 +0100

    fix import statements in example

--- examples/toolkit/pyaedt_toolkit/ui/windows/plot_design/plot_design_menu.py
+++ examples/toolkit/pyaedt_toolkit/ui/windows/plot_design/plot_design_menu.py
@@ -1,17 +1,10 @@
-import time
-
-from PySide6.QtCore import Slot
-from PySide6.QtCore import QObject
 from PySide6.QtCore import QThread
 from PySide6.QtCore import Signal
-from PySide6.QtWidgets import QComboBox
 from PySide6.QtWidgets import QLabel
-from PySide6.QtWidgets import QLineEdit
-from PySide6.QtWidgets import QVBoxLayout
 from PySide6.QtWidgets import QGridLayout
 from PySide6.QtWidgets import QWidget
-from windows.plot_design.plot_design_column import Ui_LeftColumn
-from windows.plot_design.plot_design_page import Ui_Plot_Design
+from plot_design_column import Ui_LeftColumn
+from plot_design_page import Ui_Plot_Design
 
 import tempfile
 from pyvistaqt import BackgroundPlotter
