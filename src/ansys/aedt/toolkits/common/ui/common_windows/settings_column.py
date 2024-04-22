e1d0a51f894723953a5e685b5fcc23b33621b4e2
Samuelopez-ansys <samuel.lopez@ansys.com>
2024-04-22 14:47:48 +0200

    Fix bug when toolkit is launched from AEDT

--- src/ansys/aedt/toolkits/common/ui/common_windows/settings_column.py
+++ src/ansys/aedt/toolkits/common/ui/common_windows/settings_column.py
@@ -172,7 +172,7 @@ class SettingsMenu(QObject):
         success = self.app.check_connection()
         if success:
             backend_properties = self.app.get_properties()
-            if backend_properties.get("selected_process"):
+            if backend_properties.get("selected_process") != 0:
                 self.connect_aedt_directly()
             else:
                 self.connect_aedt.clicked.connect(self.launch_aedt)
