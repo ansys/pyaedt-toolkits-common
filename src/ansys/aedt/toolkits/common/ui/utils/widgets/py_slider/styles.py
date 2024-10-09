fd721331ea8ed46e4b874f958fe6889cd7027ee2
Samuelopez-ansys <Samuelopez-ansys@users.noreply.github.com>
2024-10-09 15:23:06 +0000

    DOC: update development documentation 49d1c3355376675d11bcbda3dd74ed22be04b064

---
+++ src/ansys/aedt/toolkits/common/ui/utils/widgets/py_slider/styles.py
@@ -0,0 +1,41 @@
+class Styles(object):
+    style = """
+    /* HORIZONTAL */
+    QSlider {{ margin: {_margin}px; }}
+    QSlider::groove:horizontal {{
+        border-radius: {_bg_radius}px;
+        height: {_bg_size}px;
+    	margin: 0px;
+    	background-color: {_bg_color};
+    }}
+    QSlider::groove:horizontal:hover {{ background-color: {_bg_color_hover}; }}
+    QSlider::handle:horizontal {{
+        border: none;
+        height: {_handle_size}px;
+        width: {_handle_size}px;
+        margin: {_handle_margin}px;
+    	border-radius: {_handle_radius}px;
+        background-color: {_handle_color};
+    }}
+    QSlider::handle:horizontal:hover {{ background-color: {_handle_color_hover}; }}
+    QSlider::handle:horizontal:pressed {{ background-color: {_handle_color_pressed}; }}
+
+    /* VERTICAL */
+    QSlider::groove:vertical {{
+        border-radius: {_bg_radius}px;
+        width: {_bg_size}px;
+        margin: 0px;
+    	background-color: {_bg_color};
+    }}
+    QSlider::groove:vertical:hover {{ background-color: {_bg_color_hover}; }}
+    QSlider::handle:vertical {{
+    	border: none;
+        height: {_handle_size}px;
+        width: {_handle_size}px;
+        margin: {_handle_margin}px;
+    	border-radius: {_handle_radius}px;
+        background-color: {_handle_color};
+    }}
+    QSlider::handle:vertical:hover {{ background-color: {_handle_color_hover}; }}
+    QSlider::handle:vertical:pressed {{ background-color: {_handle_color_pressed}; }}
+    """
