b6e907dd655debf8547881c46ad0c177a34ca82e
Samuelopez-ansys <samuel.lopez@ansys.com>
2025-11-13 19:26:37 +0100

    Test Linux mono

--- scripts/preload_mono_libs.py
+++ scripts/preload_mono_libs.py
@@ -6,6 +6,12 @@ mono_dir = os.environ.get("MONO_DIR")
 if mono_dir:
     loaded = 0
     for filename in os.listdir(mono_dir):
+        # Only load .so files
+        if not filename.endswith(".so"):
+            continue
+
+        fullpath = os.path.join(mono_dir, filename)
+
         try:
             ctypes.CDLL(fullpath, mode=ctypes.RTLD_GLOBAL)
             loaded += 1
