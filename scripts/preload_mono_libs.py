41e329e8d24b892eb66267a3039bb4011a99a4c2
Samuelopez-ansys <samuel.lopez@ansys.com>
2025-11-14 10:49:12 +0100

    Load only EDBCWrapper

--- scripts/preload_mono_libs.py
+++ scripts/preload_mono_libs.py
@@ -1,20 +1,29 @@
 import ctypes
 import os
 
+ansys_em = os.environ.get("ANSYS_EM")
 mono_dir = os.environ.get("MONO_DIR")
 
-if mono_dir:
+
+if mono_dir and ansys_em:
     loaded = 0
-    for filename in os.listdir(mono_dir):
-        if not filename.endswith(".so"):
-            continue
+    fullpath = os.path.join(ansys_em, "libEDBCWrapper.so")
+    try:
+        ctypes.CDLL(fullpath, mode=ctypes.RTLD_GLOBAL)
+        loaded += 1
+    except OSError as e:
+        print(f"WARNING: Failed to load {fullpath}: {e}")
 
-        fullpath = os.path.join(mono_dir, filename)
-
-        try:
-            ctypes.CDLL(fullpath, mode=ctypes.RTLD_GLOBAL)
-            loaded += 1
-        except OSError as e:
-            print(f"WARNING: Failed to load {fullpath}: {e}")
+    # for filename in os.listdir(mono_dir):
+    #     if not filename.endswith(".so"):
+    #         continue
+    #
+    #     fullpath = os.path.join(mono_dir, filename)
+    #
+    #     try:
+    #         ctypes.CDLL(fullpath, mode=ctypes.RTLD_GLOBAL)
+    #         loaded += 1
+    #     except OSError as e:
+    #         print(f"WARNING: Failed to load {fullpath}: {e}")
 
     print(f"Successfully loaded {loaded} shared libraries from {mono_dir}")
